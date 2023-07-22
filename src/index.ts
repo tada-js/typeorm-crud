664dc1c1fe2e338e392025b35a378324c3b4b2ea
tada-js <justfix010@gmail.com>
2023-07-22 15:02:51 +0900

    TypeORM으로 CRUD 구현

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import { AppDataSource } from './data-source';
+import { User } from './entity/User';
 
 const app = express();
 
@@ -18,6 +19,40 @@ AppDataSource.initialize()
   .catch((error) => {
     console.error(error);
   });
+
+app.post('/users', async (req, res) => {
+  const user = await AppDataSource.getRepository(User).create(req.body);
+  console.log(user);
+  const results = await AppDataSource.getRepository(User).save(user);
+  return res.send(results);
+});
+
+app.get('/users', async (req, res) => {
+  const results = await AppDataSource.getRepository(User).find();
+  res.json(results);
+});
+
+app.get('/users/:id', async (req, res) => {
+  const results = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+  return res.json(results);
+});
+
+app.put('/users/:id', async (req, res) => {
+  const user = await AppDataSource.getRepository(User).findOneBy({
+    id: Number(req.params.id),
+  });
+
+  AppDataSource.getRepository(User).merge(user, req.body);
+  const result = await AppDataSource.getRepository(User).save(user);
+  return res.send(result);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const result = await AppDataSource.getRepository(User).delete(req.params.id);
+  res.json(result);
+});
 
 const port = 4000;
 app.listen(port, () => {
